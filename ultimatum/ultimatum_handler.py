b4faf64c04c62dc68f6448c99e1db86434df2d1e
Janusz Lenar <malleor@gmail.com>
2023-09-09 19:41:49 +0200

    Added few more endpoints

--- ultimatum/ultimatum_handler.py
+++ ultimatum/ultimatum_handler.py
@@ -1,11 +1,41 @@
 import json
 
-def hello(event, context):
+def home(event, context):
     print('request: {}'.format(json.dumps(event)))
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/html'
         },
-        'body': 'Lorem ipsum\n'
+        'body': '<h1>New session form</h1>'
+    }
+
+def create_session(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'body': '<h1>Session admin panel</h1>'
+    }
+
+def session_lobby(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'body': '<h1>Log in to session form</h1>'
+    }
+
+def join_session(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'body': '<h1>Waiting for a next round</h1>'
     }
--- ultimatum/ultimatum_service.py
+++ ultimatum/ultimatum_service.py
@@ -7,15 +7,44 @@ class UltimatumService(Construct):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
-        handler = lambda_.Function(self, "UltimatumHandler",
+        # config lambda
+        home_handler = lambda_.Function(self, "home_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.from_asset("ultimatum"),
-                    handler="ultimatum_handler.hello"
+                    handler="ultimatum_handler.home"
+                    )
+        create_session_handler = lambda_.Function(self, "create_session_handler",
+                    runtime=lambda_.Runtime.PYTHON_3_7,
+                    code=lambda_.Code.from_asset("ultimatum"),
+                    handler="ultimatum_handler.create_session"
+                    )
+        session_lobby_handler = lambda_.Function(self, "session_lobby_handler",
+                    runtime=lambda_.Runtime.PYTHON_3_7,
+                    code=lambda_.Code.from_asset("ultimatum"),
+                    handler="ultimatum_handler.session_lobby"
+                    )
+        join_session_handler = lambda_.Function(self, "join_session_handler",
+                    runtime=lambda_.Runtime.PYTHON_3_7,
+                    code=lambda_.Code.from_asset("ultimatum"),
+                    handler="ultimatum_handler.join_session"
                     )
 
+        # config gateway
         api = apigateway.RestApi(self, "ultimatum-api",
                   rest_api_name="Ultimatum Service",
                   description="This service handles the Ultimatum app.")
-        get_widgets_integration = apigateway.LambdaIntegration(handler,
-                request_templates={"application/json": '{ "statusCode": "200" }'})
-        api.root.add_method("GET", get_widgets_integration)
+        home_integration = apigateway.LambdaIntegration(home_handler)
+        create_session_integration = apigateway.LambdaIntegration(create_session_handler)
+        session_lobby_integration = apigateway.LambdaIntegration(session_lobby_handler)
+        join_session_integration = apigateway.LambdaIntegration(join_session_handler)
+        # /
+        api.root.add_method("GET", home_integration)
+        # /session
+        session_api = api.root.add_resource('session')
+        session_api.add_method("GET", create_session_integration)
+        # /session/login
+        login_api = session_api.add_resource('login')
+        login_api.add_method("GET", session_lobby_integration)
+        # /session/join
+        join_api = session_api.add_resource('join')
+        join_api.add_method("GET", join_session_integration)
