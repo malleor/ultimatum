fc3677b3c8b793155edb3011ff5663d052a43c66
Janusz Lenar <malleor@gmail.com>
2023-09-10 12:00:04 +0200

    Added an S3 bucket to store the static frontend

--- ultimatum/ultimatum_service.py
+++ ultimatum/ultimatum_service.py
@@ -1,13 +1,15 @@
 import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk import (aws_apigateway as apigateway,
-                     aws_lambda as lambda_)
+                     aws_lambda as lambda_,
+                     aws_s3 as s3,
+                     aws_s3_deployment as s3_deployment)
 
 class UltimatumService(Construct):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
-        # config lambda
+        # config lambda for the backend
         home_handler = lambda_.Function(self, "home_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.from_asset("ultimatum"),
@@ -29,7 +31,7 @@ class UltimatumService(Construct):
                     handler="ultimatum_handler.join_session"
                     )
 
-        # config gateway
+        # config gateway for handling traffic
         api = apigateway.RestApi(self, "ultimatum-api",
                   rest_api_name="Ultimatum Service",
                   description="This service handles the Ultimatum app.")
@@ -48,3 +50,17 @@ class UltimatumService(Construct):
         # /session/join
         join_api = session_api.add_resource('join')
         join_api.add_method("GET", join_session_integration)
+
+        # configure S3 for static frontend
+        bucket = s3.Bucket(
+            self,
+            id="frontend",
+            block_public_access=s3.BlockPublicAccess(block_public_policy=False),
+            public_read_access=True,
+            website_index_document="index.html"
+            )
+        s3_deployment.BucketDeployment(
+            self,
+            id="bucket_deployment",
+            sources=[s3_deployment.Source.asset("ultimatum/frontend")],
+            destination_bucket=bucket)
