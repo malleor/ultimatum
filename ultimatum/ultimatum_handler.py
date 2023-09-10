4a527a7e46978a4a8a51bd26bf522100c3a7b1eb
Janusz Lenar <malleor@gmail.com>
2023-09-10 17:35:09 +0200

    Clean up the endpoints - only create and join session

--- ultimatum/ultimatum_handler.py
+++ ultimatum/ultimatum_handler.py
@@ -1,33 +1,13 @@
 import json
 
-def home(event, context):
-    print('request: {}'.format(json.dumps(event)))
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/html'
-        },
-        'body': '<h1>New session form</h1>'
-    }
-
 def create_session(event, context):
     print('request: {}'.format(json.dumps(event)))
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/json'
         },
-        'body': '<h1>Session admin panel</h1>'
-    }
-
-def session_lobby(event, context):
-    print('request: {}'.format(json.dumps(event)))
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/html'
-        },
-        'body': '<h1>Log in to session form</h1>'
+        'body': json.dumps({'session_id': 'XXXX'})
     }
 
 def join_session(event, context):
@@ -35,7 +15,7 @@ def join_session(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/json'
         },
-        'body': '<h1>Waiting for a next round</h1>'
+        'body': json.dumps({'websocket_topic': 'XXXX'})
     }
--- ultimatum/ultimatum_service.py
+++ ultimatum/ultimatum_service.py
@@ -10,20 +10,10 @@ class UltimatumService(Construct):
         super().__init__(scope, id)
 
         # config lambda for the backend
-        home_handler = lambda_.Function(self, "home_handler",
-                    runtime=lambda_.Runtime.PYTHON_3_7,
-                    code=lambda_.Code.from_asset("ultimatum"),
-                    handler="ultimatum_handler.home"
-                    )
         create_session_handler = lambda_.Function(self, "create_session_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.from_asset("ultimatum"),
                     handler="ultimatum_handler.create_session"
-                    )
-        session_lobby_handler = lambda_.Function(self, "session_lobby_handler",
-                    runtime=lambda_.Runtime.PYTHON_3_7,
-                    code=lambda_.Code.from_asset("ultimatum"),
-                    handler="ultimatum_handler.session_lobby"
                     )
         join_session_handler = lambda_.Function(self, "join_session_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
@@ -35,21 +25,14 @@ class UltimatumService(Construct):
         api = apigateway.RestApi(self, "ultimatum-api",
                   rest_api_name="Ultimatum Service",
                   description="This service handles the Ultimatum app.")
-        home_integration = apigateway.LambdaIntegration(home_handler)
         create_session_integration = apigateway.LambdaIntegration(create_session_handler)
-        session_lobby_integration = apigateway.LambdaIntegration(session_lobby_handler)
         join_session_integration = apigateway.LambdaIntegration(join_session_handler)
-        # /
-        api.root.add_method("GET", home_integration)
-        # /session
+        # POST /session
         session_api = api.root.add_resource('session')
-        session_api.add_method("GET", create_session_integration)
-        # /session/login
-        login_api = session_api.add_resource('login')
-        login_api.add_method("GET", session_lobby_integration)
-        # /session/join
+        session_api.add_method("POST", create_session_integration)
+        # POST /session/join
         join_api = session_api.add_resource('join')
-        join_api.add_method("GET", join_session_integration)
+        join_api.add_method("POST", join_session_integration)
 
         # configure S3 for static frontend
         bucket = s3.Bucket(
