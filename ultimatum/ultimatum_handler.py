59cc8c8beefdb102c9d0e0a3990b18db2cd836e0
Janusz Lenar <malleor@gmail.com>
2023-09-17 14:03:36 +0200

    Added a database

--- ultimatum/ultimatum_handler.py
+++ ultimatum/ultimatum_handler.py
@@ -1,4 +1,7 @@
 import json
+import os
+import uuid
+import boto3
 
 common_headers = {
     'Content-Type': 'text/json',
@@ -7,10 +10,26 @@ common_headers = {
 
 def create_session(event, context):
     print('request: {}'.format(json.dumps(event)))
+
+    db = boto3.resource('dynamodb', region_name='eu-central-1')
+
+    table_name = os.environ.get('TABLE_NAME')
+    print('table name:', table_name)
+
+    new_session_id = str(uuid.uuid4().hex)
+    table = db.Table(table_name)
+    put_res = table.put_item(
+        Item={
+            'id': new_session_id
+        }
+    )
+    print('created a new session:', new_session_id)
+    print(json.dumps(put_res, indent=2))
+
     return {
         'statusCode': 200,
         'headers': common_headers,
-        'body': json.dumps({'session_id': 'XXXX'})
+        'body': json.dumps({'session_id': new_session_id})
     }
 
 def join_session(event, context):
--- ultimatum/ultimatum_service.py
+++ ultimatum/ultimatum_service.py
@@ -3,23 +3,37 @@ from constructs import Construct
 from aws_cdk import (aws_apigateway as apigateway,
                      aws_lambda as lambda_,
                      aws_s3 as s3,
-                     aws_s3_deployment as s3_deployment)
+                     aws_s3_deployment as s3_deployment,
+                     aws_dynamodb as dynamodb)
 
 class UltimatumService(Construct):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
+        # configure a database
+        table = dynamodb.Table(
+            self,
+            'Sessions',
+            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+        table_name = table.table_name
+
         # config lambda for the backend
         create_session_handler = lambda_.Function(self, "create_session_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.from_asset("ultimatum"),
-                    handler="ultimatum_handler.create_session"
+                    handler="ultimatum_handler.create_session",
+                    environment={'TABLE_NAME': table_name}
                     )
         join_session_handler = lambda_.Function(self, "join_session_handler",
                     runtime=lambda_.Runtime.PYTHON_3_7,
                     code=lambda_.Code.from_asset("ultimatum"),
-                    handler="ultimatum_handler.join_session"
+                    handler="ultimatum_handler.join_session",
+                    environment={'TABLE_NAME': table_name}
                     )
+        table.grant_full_access(create_session_handler)
+        table.grant_full_access(join_session_handler)
 
         # config gateway for handling traffic
         api = apigateway.RestApi(self, "ultimatum-api",
